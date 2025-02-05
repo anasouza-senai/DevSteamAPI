5a0852b24500e0c855373d9a0efbbf7b78e10cd6
Aninha <aninha.clara030807@gmail.com>
2025-02-05 16:06:14 -0300

    Adicionado os CONTROLADORES

--- DevSteamAPI/Data/DevSteamAPIContext.cs
+++ DevSteamAPI/Data/DevSteamAPIContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DevSteamAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevSteamAPI.Data
@@ -9,10 +10,17 @@ namespace DevSteamAPI.Data
         public DevSteamAPIContext(DbContextOptions<DevSteamAPIContext> options) : base(options)
         {
         }
+
+        // Definir as tabelas do banco de dados
+        public DbSet<Jogo> Jogos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
         //Sobrescrever o metodo OnModelCreating
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Jogo>().ToTable("Jogos");
+            modelBuilder.Entity<Categoria>().ToTable("Categorias");
         }
     }
 }
